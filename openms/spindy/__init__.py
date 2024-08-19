71e2c271c1f908405fa7c30c7a1b6469b7cc6f1c
Yu Zhang <ares201005@gmail.com>
2024-08-19 10:50:08 -0600

    formatting in spindy __init__

--- openms/spindy/__init__.py
+++ openms/spindy/__init__.py
@@ -1,14 +1,14 @@
 #
 # @ 2023. Triad National Security, LLC. All rights reserved.
 #
-# This program was produced under U.S. Government contract 89233218CNA000001 
-# for Los Alamos National Laboratory (LANL), which is operated by Triad 
-# National Security, LLC for the U.S. Department of Energy/National Nuclear 
-# Security Administration. All rights in the program are reserved by Triad 
-# National Security, LLC, and the U.S. Department of Energy/National Nuclear 
-# Security Administration. The Government is granted for itself and others acting 
-# on its behalf a nonexclusive, paid-up, irrevocable worldwide license in this 
-# material to reproduce, prepare derivative works, distribute copies to the 
+# This program was produced under U.S. Government contract 89233218CNA000001
+# for Los Alamos National Laboratory (LANL), which is operated by Triad
+# National Security, LLC for the U.S. Department of Energy/National Nuclear
+# Security Administration. All rights in the program are reserved by Triad
+# National Security, LLC, and the U.S. Department of Energy/National Nuclear
+# Security Administration. The Government is granted for itself and others acting
+# on its behalf a nonexclusive, paid-up, irrevocable worldwide license in this
+# material to reproduce, prepare derivative works, distribute copies to the
 # public, perform publicly and display publicly, and to permit others to do so.
 #
 # Author: Yu Zhang <zhy@lanl.gov>
@@ -26,27 +26,27 @@ Following techniques are implemented:
   - Cluster correlation expansion (CCE)
   - quantum embedding for normal modes
 
-  
 Theoretical background of CCE
 -----------------------------
+
 TBA
 
 
 Theoretical background of embedding
 -----------------------------------
+
 TBA
 """
 
-from . import bath
-from . import gtensorlibs
-from . import spin
-from . import system
+# from . import bath
+# from . import gtensorlibs
+# from . import spin
+# from . import system
 
 # use qutip
-try:
-    import qutip
-    QUTIP_AVAILABLE = True
-
-except ImportError as e:
-    print(f"Could not import 'qutip': {e}")
-    QUTIP_AVAILABLE = False
+# try:
+#    import qutip
+#    QUTIP_AVAILABLE = True
+# except ImportError as e:
+#    print(f"Could not import 'qutip': {e}")
+#    QUTIP_AVAILABLE = False
